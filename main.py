8176c265876f16e5d44fd31eaf2860666c09fad7
Luke Lee <durdenmisc@gmail.com>
2011-03-22 19:07:27 -0500

    Handle register post request

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-#!/usr/bin/env python
+"""
+Main Views
+"""
 
-import os
+import os, cgi
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util, template
 
 
 TEMPLATE_DIR=os.path.join(os.path.dirname(__file__), 'templates/')
+
 
 class LocalHandler(webapp.RequestHandler):
     """Silly wrapper to provide cleaner template rendering API"""
@@ -25,21 +28,37 @@ class MainHandler(LocalHandler):
         """GET request"""
         self.render_template('index.html')
 
+
 class RegisterHandler(LocalHandler):
     """Deal with registering user with local service"""
 
-    def get(self):
+    def get(self, result=None):
         """GET request"""
-        self.render_template('register.html')
 
-    def post(self):
+        msg = ""
+        if result is not None:
+            if result == "success":
+                msg = "User registered!"
+            elif result == "failed":
+                msg = "Please fill out all required fields"
+
+        self.render_template('register.html', msg=msg)
+
+    def post(self, result=None):
         """POST request"""
 
+        user_name = cgi.escape(self.request.get('user_name'))
+        if user_name == '' or not len(user_name):
+            self.redirect('/register/failed')
+        else:
+            self.redirect('/register/success')
+        
 def main():
     """main"""
 
+    #FIXME: Need a catch-all for 404 error
     application = webapp.WSGIApplication([('/', MainHandler),
-                                           ('/register/', RegisterHandler),
+                                           ('/register/(.*)', RegisterHandler),
                                         ], debug=True)
     util.run_wsgi_app(application)
 
