1dfdeeddf81aa55d867f63e9c7555a2c51560b34
Luke Lee <durdenmisc@gmail.com>
2011-05-03 21:58:32 -0500

    PEP8 cleanup

--- console.py
+++ console.py
@@ -19,6 +19,7 @@ sys.path.append(install_dir + "/lib/fancy_urllib/")
 from google.appengine.ext.remote_api import remote_api_stub
 from google.appengine.ext import db
 
+
 def auth_func():
     return raw_input('Username:'), getpass.getpass('Password:')
 
@@ -32,6 +33,8 @@ if len(sys.argv) > 2:
 else:
     host = '%s.appspot.com' % app_id
 
-remote_api_stub.ConfigureRemoteDatastore(app_id, '/remote_api', auth_func, host)
+remote_api_stub.ConfigureRemoteDatastore(app_id, '/remote_api', auth_func,
+                                         host)
 
-code.interact('App Engine interactive console for %s' % (app_id,), None, locals())
+code.interact('App Engine interactive console for %s' % (app_id,), None,
+                                                        locals())
--- main.py
+++ main.py
@@ -12,7 +12,8 @@ def main():
     """main"""
 
     from app.handlers.static import Main, About, Missing
-    from app.handlers.register import Register, RegisterCallback, Signin, SigninCallback, Logout
+    from app.handlers.register import Register, RegisterCallback
+    from app.handlers.register import Signin, SigninCallback, Logout
     from app.handlers.vote import Vote, VoteUp, VoteDown, MyVotes
 
     logging.getLogger().setLevel(logging.DEBUG)
